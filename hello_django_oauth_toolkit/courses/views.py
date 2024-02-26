ea6aa03bde42621908c8260373c6324549c1e7c1
Kan Ouivirach <kan@odds.team>
2024-02-26 02:18:39 +0000

    Implement get and post API methods on courses

--- hello_django_oauth_toolkit/courses/views.py
+++ hello_django_oauth_toolkit/courses/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+
+
+class CourseView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def get(self, request):
+        response_data = {
+            "status": "success",
+            "courses": [
+                {
+                    "name": "Airflow",
+                },
+                {
+                    "name": "dbt"
+                },
+            ],
+        }
+        return JsonResponse(response_data)
+
+    def post(self, request):
+        body_unicode = request.body.decode("utf-8")
+        body = json.loads(body_unicode)
+        print(body)
+
+        response_data = {
+            "status": "success",
+        }
+        return JsonResponse(response_data)
--- hello_django_oauth_toolkit/hello_django_oauth_toolkit/urls.py
+++ hello_django_oauth_toolkit/hello_django_oauth_toolkit/urls.py
@@ -18,6 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from courses.views import CourseView
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("courses/", CourseView.as_view(), name="courses")
 ]
